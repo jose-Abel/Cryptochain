2b3a8a715820420aa02cec65a7847d4a7ff75b0e
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-02-06 18:27:19 -0400

    Add a guard clause to prevent difficulty jumps bigger than 1

--- blockchain.js
+++ blockchain.js
@@ -49,12 +49,16 @@ class Blockchain {
             const {timestamp, lastHash, hash, nonce, difficulty, data}  = chain[i];
             
             const actualLastHash = chain[i-1].hash;
+
+            const lastDifficulty = chain[i-1].difficulty;
             
             if(lastHash !== actualLastHash) return false;
             
             const validatedHash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
             
             if(hash !== validatedHash) return false;
+
+            if(Math.abs(lastDifficulty - difficulty) > 1) return false;
         }
         
         return true;
