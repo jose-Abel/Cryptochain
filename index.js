025411670080e32aa43d8302fd5bc0f1b8f107ee
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-02-11 20:55:58 -0400

    Add the route for post request in the express server index.js file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const bodyParser = require("body-parser");
+
 const express = require("express");
 
 const Blockchain = require("./blockchain");
@@ -6,10 +8,19 @@ const app = express();
 
 const blockchain = new Blockchain();
 
+app.use(bodyParser.json());
+
 app.get("/api/blocks", (req, res)=> {
   res.json(blockchain.chain);
 });
 
+app.post("/api/mine", (req, res)=> {
+  const { data } = req.body
+
+  blockchain.addBlock({data});
+
+  res.redirect("/api/blocks");  
+});
 
 const PORT = 3000;
 
