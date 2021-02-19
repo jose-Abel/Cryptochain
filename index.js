babba4d554de5c8f9fe18754129b48ada27665d9
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-02-19 16:31:13 -0400

    Update wallet/index.test.js to add the tests to calculate the accurate balance of the wallet base on the recent trasanction

--- index.js
+++ index.js
@@ -61,7 +61,10 @@ app.post("/api/transact", (req, res) => {
       transaction.update({ senderWallet: wallet, recipient, amount });
 
     } else {
-      transaction = wallet.createTransaction({ recipient, amount });
+      transaction = wallet.createTransaction({ 
+        recipient, 
+        amount, 
+        chain: blockchain.chain });
     }
   }
   catch(err) {
