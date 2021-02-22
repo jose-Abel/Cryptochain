5262185c58d561c7104a048b0dde62fd77425acc
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-02-21 22:33:57 -0400

    Update the blockchain/index.test.js to add one last test for the validTransactionData method in the blockchain class to check that the blockchain has a unique set of transactions so no transaction can appear more than once on the block. Update the blockchain/index.js class to add the logic to pass the tests

--- blockchain/index.test.js
+++ blockchain/index.test.js
@@ -278,7 +278,13 @@ describe('Blockchain', ()=> {
         });
 
         describe("and a block contains multiple identical transaction", () => {
-            it("returns false and logs an error", () => {});
+            it("returns false and logs an error", () => {
+                newChain.addBlock({data: [transaction, transaction, transaction]});
+
+                expect(blockchain.validTransactionData({chain: newChain.chain})).toBe(false);
+                
+                expect(errorMock).toHaveBeenCalled();                
+            });
         });
     });
 });
